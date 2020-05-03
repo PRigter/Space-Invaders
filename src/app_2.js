55036c058608a1791b709fbf73f32f803cb39c14
Paulo Rigter <paulorigter@gmail.com>
2020-05-03 20:18:53 +0100

    OPP added MODEL + general behaviors

--- src/app_2.js
+++ src/app_2.js
@@ -12,7 +12,6 @@ let view = {
     alienInvadersTakenDown: [],
     result: 0,
     direction: 1,
-    invaderId: 0,
     
     get lastDefenseLine() {
         return squares.length - (this.width * 2)
@@ -37,25 +36,61 @@ let view = {
 }
 
 
+
+let model = {
+    invaderId: function() {
+        setInterval(this.aliensMove, 500)
+    },
+
+    aliensMove: function() {
+        const leftEdge = view.alienInvaders[0] % view.width === 0
+        const rightEdge = view.alienInvaders[view.alienInvaders.lenght - 1] % view.width === view.width -1
+        
+        if ((leftEdge && view.direction === -1) || (rightEdge && view.direction === 1)) {
+            view.direction = view.width
+        } else if (view.direction === view.width) {
+            if (leftEdge) {
+                view.direction = 1
+            } else {
+                view.direction -1
+            }
+        }
+
+        console.log(view.alienInvaders.length)
+
+        for (let i = 0; i <= view.alienInvaders.length -1; i++) {
+            squares[view.alienInvaders[i]].classList.remove("invader")
+        }
+        for (let i = 0; i <= view.alienInvaders.length -1; i++) {
+            view.alienInvaders[i] += direction
+        }
+        
+    }
+}
+
+
 let controller = {
-    shooterMove: function (e) {
+    shooterMove: function(e) {
+        squares[view.currentShooterIndex].classList.remove("shooter")
+
         switch (e.keyCode) {
             case 37:
-                if (currentShooterIndex % width !== 0) {
-                    currentShooterIndex -= 1
+                console.log("left")
+                if (view.currentShooterIndex % view.width !== 0) {
+                    view.currentShooterIndex -= 1
                 }
                 break
             case 39:
-                if (currentShooterIndex % width < width - 1) {
-                    currentShooterIndex += 1
+                console.log("right")
+                if (view.currentShooterIndex % view.width < view.width - 1) {
+                    view.currentShooterIndex += 1
                 }
                 break
         }
+
+        squares[view.currentShooterIndex].classList.add("shooter")
     },  
     
-    aliensMove: function() {
-        
-    },
 
     fire: function(e) {
         if (e.keyCode === 32) {
@@ -65,6 +100,7 @@ let controller = {
 }
 
 
+document.addEventListener("keydown", controller.shooterMove)
 document.addEventListener("keyup", controller.fire)
 
 
@@ -83,4 +119,6 @@ function init() {
     view.resultDisplay()
     view.shooterDisplay()
     view.aliensDisplay()
+    model.invaderId()
+
 }
